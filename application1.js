637f0ca6f44096a04c8cb200dfaeb9db23362a3e
Brittany Pugh <pughbritt00@gmail.com>
2016-12-06 21:58:36 -0500

    hide all divs except for intro and add functionality for btn1a and btn1b

--- application1.js
+++ application1.js
@@ -1,5 +1,14 @@
 var main = function () {
-    //document.write("hello world");
+    $(".choice").toggle();
+    
+    $("#btn1a").click(function(){
+        $("#choice1a").toggle(); 
+    });
+    
+    $("#btn1b").click(function(){
+        $("#choice1b").toggle();
+    });
+
 };
 
 $(document).ready(main);
