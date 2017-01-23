90510390070b2368de94af072b9341c8886ab47b
Brittany Pugh <pughbritt00@gmail.com>
2017-01-23 18:36:56 -0500

    use IDs for buttons to add new choices

--- application1.js
+++ application1.js
@@ -1,13 +1,23 @@
 var main = function () {
     $(".choice").toggle();
     
-    $("#btn1a").click(function(){
+    $("#btn1a").click(function(){ //cook something
         $("#choice1a").toggle(); 
     });
     
-    $("#btn1b").click(function(){
-        $("#choice1b").toggle();
+    $("#btn1b").click(function(){ //buy something
+        $("#choice1b").toggle(); 
     });
+    
+    $("#btn2a").click(function(){ //salad
+        $("#choice2a").toggle();
+    });
+    
+    $("#btn2b").click(function(){ //cupcakes
+        $("#choice2b").toggle();
+    });
+    
+    $()
 
 };
 
